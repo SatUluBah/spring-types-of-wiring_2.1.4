efc1c0421aba80b8846f472e36fb1f467f05ab42
UlugbekSatymov <satymov.u@gmail.com>
2020-12-11 02:39:06 +0300

    2.1.4

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,13 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    private String str = "";
+
+    @Override
+    public String toString() {
+        return str;
+    }
 }
